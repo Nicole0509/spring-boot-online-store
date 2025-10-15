2d977f304a1b31700700cbf49faf6adfae9e9787
Nicole0509 <niashavril0509@gmail.com>
2025-10-15 21:30:14 +0200

    Using a setter to inject dependencies - recommended for optional dependencies only otherwise use constructors instead

--- src/main/java/org/example/store/OrderService.java
+++ src/main/java/org/example/store/OrderService.java
@@ -3,11 +3,15 @@ package org.example.store;
 public class OrderService {
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService){
-        this.paymentService = paymentService;
-    }
+//    public OrderService(PaymentService paymentService){
+//        this.setPaymentService(paymentService);
+//    }
 
     public void placeOrder(){
         paymentService.processPayment(10);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
