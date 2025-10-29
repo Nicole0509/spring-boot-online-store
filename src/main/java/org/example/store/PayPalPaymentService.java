dcd45a23b06bcb392278d4ef5d8eb4eab05dd1cb
Nicole0509 <niashavril0509@gmail.com>
2025-10-29 09:01:47 +0200

    Created a users entity and lombok to initialize getters and setter

--- src/main/java/org/example/store/PayPalPaymentService.java
+++
@@ -1,9 +0,0 @@
-package org.example.store;
-
-public class PayPalPaymentService implements PaymentService{
-    @Override
-    public void processPayment(double amount){
-        System.out.println("PAYPAL PAYMENT");
-        System.out.println("Order amount: " + amount);
-    }
-}
--- src/main/java/org/example/store/PaymentService.java
+++
@@ -1,5 +0,0 @@
-package org.example.store;
-
-public interface PaymentService {
-    void processPayment(double amount);
-}
